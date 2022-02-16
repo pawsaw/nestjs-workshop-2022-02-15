a4377356cf8da5f4fa1b64b33c4bad6003a6f5d4
Pawel Sawicki <public@psawicki.de>
2022-02-16 10:35:24 +0100

    Adding Open-API

--- src/books/book.ts
+++ src/books/book.ts
@@ -1,14 +1,33 @@
-export interface Publisher {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Publisher {
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   url: string;
 }
 
-export interface Book {
+export class Book {
+  @ApiProperty()
   title: string;
+
+  @ApiProperty()
   subtitle: string;
+
+  @ApiProperty()
   isbn: string;
+
+  @ApiProperty()
   abstract: string;
+
+  @ApiProperty()
   numPages: number;
+
+  @ApiProperty()
   author: string;
+
+  @ApiProperty({
+    description: 'The publisher of the book',
+  })
   publisher: Publisher;
 }
--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { Book } from './book';
 import { books } from './books';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 
 @Controller('books')
+@ApiTags('books')
 export class BooksController {
+  @ApiResponse({
+    description: 'Returns the complete collection of books',
+    status: HttpStatus.OK,
+    type: [Book],
+  })
   @Get()
   findAll(): Book[] {
     return books;
