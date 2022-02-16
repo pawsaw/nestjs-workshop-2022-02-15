80aacc9386cb11bb4413ddca0896e2021068c57e
Pawel Sawicki <public@psawicki.de>
2022-02-16 14:58:48 +0100

    BooksService

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,21 +1,14 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpStatus,
-  NotFoundException,
-  Param,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post } from '@nestjs/common';
 import { Book, ISBN } from './book';
 import { ApiTags, ApiResponse } from '@nestjs/swagger';
-import { getBooksAsync } from './getBooksAsync';
 import { CreateBookDto } from './create-book.dto';
-import { books } from './books';
+import { BooksService } from './books.service';
 
 @Controller('books')
 @ApiTags('books')
 export class BooksController {
+  constructor(private booksService: BooksService) {}
+
   @ApiResponse({
     description: 'Returns the complete collection of books',
     status: HttpStatus.OK,
@@ -23,37 +16,7 @@ export class BooksController {
   })
   @Get()
   findAll(): Promise<Book[]> {
-    return getBooksAsync();
-  }
-
-  @ApiResponse({
-    description: 'Returns books containing the requested title token',
-    status: HttpStatus.OK,
-    type: [Book],
-  })
-  @Get('/byTitle/:title')
-  async findByTitle(@Param('title') title: string): Promise<Book[]> {
-    const allBooks = await getBooksAsync();
-    const filteredBooks = allBooks.filter((book) =>
-      book.title.toLowerCase().includes(title.toLowerCase()),
-    );
-    const sortedBooks = filteredBooks.sort(
-      (a: Book, b: Book) => a.title.length - b.title.length,
-    );
-
-    return sortedBooks;
-
-    // return getBooksAsync()
-    //   .then((allBooks) => {
-    //     return allBooks.filter((book) =>
-    //       book.title.toLowerCase().includes(title.toLowerCase()),
-    //     );
-    //   })
-    //   .then((filteredBooks) => {
-    //     return filteredBooks.sort(
-    //       (a: Book, b: Book) => a.title.length - b.title.length,
-    //     );
-    //   });
+    return this.booksService.findAll();
   }
 
   @ApiResponse({
@@ -70,31 +33,11 @@ export class BooksController {
     @Param('isbn') isbn: ISBN,
     // @Res() response: Response,
   ): Promise<Book> {
-    const allBooks = await getBooksAsync();
-    const book = allBooks.find((book) => book.isbn === isbn);
-    if (!book) {
-      throw new NotFoundException('Book with requested isbn not found');
-      //   response
-      //     .status(HttpStatus.NOT_FOUND)
-      //     .send('Book with requested isbn not found');
-    }
-
-    // response.status(HttpStatus.OK).json(book);
-
-    return book;
+    return this.booksService.findOne(isbn);
   }
 
   @Post()
   async createBook(@Body() createBookDto: CreateBookDto): Promise<Book> {
-    const newBook: Book = {
-      ...createBookDto,
-      publisher: {
-        ...createBookDto.publisher,
-      },
-    };
-
-    books.push(newBook);
-
-    return newBook;
+    return this.booksService.createBook(createBookDto);
   }
 }
